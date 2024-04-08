dfae7b7c11f3d94fa6124c8f9dfd3c7f1e57343a
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2024-04-08 20:10:00 +0300

    add swagger

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Palette API Documentation",
+        default_version='v1',
+        description="API description",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="mapaxomov@yandex.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('palette/', include('palette.urls', namespace='palette')),
     path('users/', include('users.urls', namespace='users')),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
